7dbc1f688009cf212b891b042f60af44b0fc76ac
Christian Schilling <christian.schilling@rub.de>
2013-02-19 10:25:50 +0100

    map improvements

--- cl_meta.py
+++ cl_meta.py
@@ -125,6 +125,7 @@ def symbolic(func):
                 newargs.append(arg)
 
         symbolic_result = func(*newargs)
+
         lambdified = sympy.lambdify(symargs,symbolic_result,CLExpression)
         r = lambdified(*[CLExpression(x) for x in expargs])
         return r
@@ -132,40 +133,65 @@ def symbolic(func):
     def sy(*args):
         return func(*args)
 
-    def itersym(ctx,*args,**kwargs):
+    def itersym(self,ctx,*args,**kwargs):
         return iterate_symbolic(ctx,f,*args,**kwargs)
 
+    import types
     f.symbolic = sy
-    f.elementwise = itersym
+    f.map = types.MethodType(itersym,f,type(f))
+    f.elementwise = f.map
     return f
 
 
 
-def iterate_symbolic(ctx,func,iterate=1,input=[],output=[]):
+def iterate_symbolic(ctx,func,iterations=1,input=[],output=[],output_indices=None,input_indices=None,assignment='='):
     from pyopencl.elementwise import ElementwiseKernel
+    from pyopencl.array import Array
+
     unique = []
     i=0
     for x in input+output:
-        if not hasattr(x,'paramname'):
+        if not hasattr(x,'paramname') and isinstance(x,Array):
             unique.append(x)
             x.paramname = 'param{}'.format(i)
             i+=1
 
+    out_indices = ['i' for x in output]
+    if output_indices:
+        out_indices = map(str,output_indices(CLExpression(len(unique[0])),CLExpression('i'),CLExpression('iteration')))
+
+    in_indices = ['i' for x in input if hasattr(x,'paramname')]
+    if input_indices:
+        in_indices = map(str,input_indices(CLExpression(len(unique[0])),CLExpression('i'),CLExpression('iteration')))
+
+    for i,index in enumerate(in_indices):
+        in_indices[i] = '({i}>=0?{i}:0)'.format(i=index)
+
+    for i,index in enumerate(in_indices):
+        in_indices[i] = '({i}<{l}?{i}:({l}-1))'.format(i=index,l=len(output[0]))
+
+    if isinstance(assignment,str):
+        assignment = [assignment]*len(output)
+
+    #print in_indices
+
+
     paramlist = ','.join(["float* {}".format(x.paramname) for x in unique])
-    code = ';'.join(["float tmp_{}={}[i]".format(x.paramname,x.paramname) for x in input])
-    #print code
+    code = ';'.join(["float tmp_{}_{}={}[{}]".format(i,x.paramname,x.paramname,ind) for i,ind,x in zip(range(len(input)),in_indices,input) if hasattr(x,'paramname')])
     code += ';'
-    code += ';'.join(["{}[i]={{}}".format(x.paramname,x.paramname) for x in output])
-    #print code
+    code += ';'.join(["{}[{}]{}{{}}".format(x.paramname,i,a,x.paramname) for i,a,x in zip(out_indices,assignment,output)])
     code += ';'
-    code = code.format(*func(*['tmp_'+x.paramname for x in input]))
-    code = "for(int j=0;j<{};j++){{{}}}".format(iterate,code)
-    #print paramlist
-    #print code
+    func_result = func(*['tmp_{}_{}'.format(i,x.paramname) if hasattr(x,'paramname') else x for i,x in enumerate(input)])
+    if not isinstance(func_result,list):
+        raise ValueError("symbolic function must return a list")
+    code = code.format(*func_result)
+    code = "for(int iteration=0;iteration<{};iteration++){{{}}}".format(iterations,code)
     kernel = ElementwiseKernel(ctx,paramlist,code,"update")
-    def updater(queue):
+    def updater(queue=None):
         kernel(*unique)
     updater.code = code
     for x in unique:
         del x.paramname
     return updater
+
+symbolic.map = iterate_symbolic
---
+++ tests/elementwise.py
@@ -0,0 +1,171 @@
+import unittest
+import pyopencl as cl
+import pyopencl.array
+from cl_meta import symbolic,iterate_symbolic
+import numpy
+
+class TestElementwise(unittest.TestCase):
+    def setUp(self):
+        self.ctx = cl.create_some_context()
+        self.queue = cl.CommandQueue(self.ctx)
+
+    def tearDown(self):
+        del self.queue
+        del self.ctx
+
+
+    def test_elementwise(self):
+
+        @symbolic
+        def add(a,b):
+            return [a+b]
+
+        A = cl.array.zeros(self.queue,10,dtype='float32')+1
+        B = cl.array.zeros(self.queue,10,dtype='float32')+1
+        C = cl.array.zeros(self.queue,10,dtype='float32')+1
+
+        adder = add.elementwise(self.ctx,input=[A,B],output=[C])
+
+        adder()
+
+        self.assertEqual(list(C.get()),[2.0]*10)
+
+    def test_elementwise_with_constant(self):
+
+        @symbolic
+        def add(a,b):
+            return [a+b]
+
+        A = cl.array.zeros(self.queue,10,dtype='float32')+1
+        C = cl.array.zeros(self.queue,10,dtype='float32')+1
+
+        adder = add.elementwise(self.ctx,input=[A,3],output=[C])
+
+        adder()
+
+        self.assertEqual(list(C.get()),[4.0]*10)
+
+    def test_elementwise_incremental(self):
+
+        @symbolic
+        def add(a,b):
+            return [a+b]
+
+        A = cl.array.zeros(self.queue,10,dtype='float32')+1
+        B = cl.array.zeros(self.queue,10,dtype='float32')+1
+        C = cl.array.zeros(self.queue,10,dtype='float32')
+
+        adder = add.elementwise(self.ctx,input=[A,B],output=[C],assignment='+=')
+        adder()
+        self.assertEqual(list(C.get()),[2.0]*10)
+        adder()
+        self.assertEqual(list(C.get()),[4.0]*10)
+
+    def test_output_is_input(self):
+
+        @symbolic
+        def add(a,b):
+            return [a+b]
+
+        A = cl.array.zeros(self.queue,10,dtype='float32')+1
+        B = cl.array.zeros(self.queue,10,dtype='float32')+1
+
+        adder = add.elementwise(self.ctx,input=[A,B],output=[A])
+
+        adder()
+
+        self.assertEqual(list(A.get()),[2.0]*10)
+
+    def test_double_input(self):
+
+        @symbolic
+        def add(a,b):
+            return [a+b]
+
+        A = cl.array.zeros(self.queue,10,dtype='float32')+1
+        B = cl.array.zeros(self.queue,10,dtype='float32')+1
+
+        adder = add.map(self.ctx,input=[A,A],output=[B])
+
+        adder()
+
+        self.assertEqual(list(B.get()),[2.0]*10)
+
+    def test_reverse(self):
+
+        A = cl.array.to_device(self.queue,numpy.arange(0,10).astype('float32'))
+        B = cl.array.empty_like(A)
+
+
+        noper = symbolic.map(self.ctx,self.nop,
+            input=[A],
+            output=[B],
+            iterations=1,
+            output_indices=lambda n,i,j: [n-i-1],
+        )
+
+        noper()
+
+        self.assertEqual(list(B.get()),range(9,-1,-1))
+
+    @symbolic
+    def nop(self,a):
+        return [a,a*2]
+
+    def test_oddeven(self):
+
+
+        A = cl.array.to_device(self.queue,numpy.array(range(5)*2).astype('float32'))
+        B = cl.array.empty_like(A)
+        C = cl.array.empty_like(A)
+
+
+        noper = symbolic.map(self.ctx,self.nop,
+            input=[A],
+            output=[B,B],
+            output_indices=lambda n,i,j: [(2*i)%n,(2*i+1)%n],
+        )
+
+        noper()
+
+        self.assertEqual(list(B.get()),[0,0,1,2,2,4,3,6,4,8])
+
+    @symbolic
+    def differece(self,a,b):
+        return [a-b]
+
+    def test_input_indices_diffent_inputs(self):
+
+
+        A = cl.array.to_device(self.queue,numpy.array(range(5)*2).astype('float32'))
+        X = cl.array.to_device(self.queue,numpy.array(range(5)*2).astype('float32'))
+        B = cl.array.empty_like(A)
+
+
+        differ = symbolic.map(self.ctx,self.differece,
+            input=[A,X],
+            output=[B],
+            input_indices=lambda n,i,j: [i+1,i-1],
+        )
+
+        differ()
+
+        self.assertEqual(list(B.get()),
+            [1., 2., 2., 2.,-3.,-3., 2., 2., 2.,1.]
+        )
+
+    def test_input_indices_same_inputs(self):
+        A = cl.array.to_device(self.queue,numpy.array(range(5)*2).astype('float32'))
+        B = cl.array.empty_like(A)
+
+        differ = symbolic.map(self.ctx,self.differece,
+            input=[A,A],
+            output=[B],
+            input_indices=lambda n,i,j: [i+1,i-1],
+        )
+
+        differ()
+
+        self.assertEqual(list(B.get()),
+            [1., 2., 2., 2.,-3.,-3., 2., 2., 2.,1.]
+        )
