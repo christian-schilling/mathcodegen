326e30b21c7cd56334e410b7db3d4a3acc3fc224
schansge <grosser.knuff@googlemail.com>
2013-02-26 21:18:56 +0100

    cl_meta function deleted

--- mathcodegen/symbolic.py
+++ mathcodegen/symbolic.py
@@ -1,20 +1,5 @@
 from expression import Expression
 
-def cl_meta(func):
-    def f(self,*args):
-        newargs = []
-        for arg in args:
-            if type(arg) is list:
-                newargs.append(map(Expression,arg))
-            else:
-                newargs.append(Expression(arg))
-        args = newargs
-        r = func(self,*args)
-        if type(r) is list:
-            r = ';'.join(map(str,r))
-        return r
-    return f
-
 def symbolic(func):
     import sympy
 
