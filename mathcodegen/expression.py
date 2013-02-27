243b2891c3878ae41822b19c30e254f1e33e8f78
Patrik Gebhardt <patrik.gebhardt@rub.de>
2013-02-27 09:57:58 +0100

    expand method generates compound expression rather and some typio

--- mathcodegen/expression.py
+++ mathcodegen/expression.py
@@ -12,7 +12,7 @@ class ExpressionMeta(type):
             # method generator
             def makeMethod(f):
                 return lambda self, *args: cls(f.format(self, *map(cls, args)),
-                self.depth + 1, self.subexpression)
+                self.recursion_depth + 1, self.subexpression)
 
             # generate method
             setattr(cls, name, makeMethod(operation))
@@ -26,22 +26,17 @@ class ExpressionMeta(type):
 class Expression:
     __metaclass__ = ExpressionMeta
 
-    def __init__(self, expression, depth=0, subexpression=None):
-        # save expression
+    def __init__(self, expression, recursion_depth=0, subexpression=None):
         self.expression = expression
-
-        # recursion depth
-        self.depth = depth;
-
-        # create subexpression
+        self.recursion_depth = recursion_depth;
         self.subexpression = subexpression
 
         # shorten expression
-        if depth >= 100:
+        if recursion_depth >= 100:
             self.subexpression = ('subexpression_{}'.format(id(self.expression)),
-                Expression(self.expression, depth - 1, self.subexpression))
+                Expression(self.expression, recursion_depth - 1, self.subexpression))
             self.expression = self.subexpression[0]
-            self.depth = 0
+            self.recursion_depth = 0
 
     def __str__(self):
         return '({})'.format(self.expression)
@@ -51,7 +46,7 @@ class Expression:
         ('pi', 'M_PI')
     ]
 
-    # operations
+    # list of operations which are turned into methods by ExpressionMeta
     operations = [
         ('__pos__','+{}'),
         ('__neg__','-{}'),
@@ -88,6 +83,8 @@ class Expression:
         ('gamma','tgamma({})'),
     ]
 
+    # pow method expands pow recursivly on int value,
+    # or uses built in mathmatical pow for other values
     def __pow__(self, value):
         if not isinstance(value, int):
             return self.pow(value)
@@ -97,18 +94,18 @@ class Expression:
         else:
             return Expression('{}*{}'.format(
                 self, self ** (value - 1)),
-                self.depth + 1, self.subexpression)
+                self.recursion_depth + 1, self.subexpression)
 
+    # enrole subexpressions to generate single expression
     def expand(self, dtype='float'):
-        # generate subexpressions
-        expressions = [('result', str(self.expression))]
-
-        # gather subexpressions
+        # generate list containing list of tuples with subexpression and
+        # its name. This list will be reversed order
+        subexpressions = []
         expression = self
         while True:
             if expression.subexpression is not None:
                 # get subexpression
-                expressions.append(
+                subexpressions.append(
                     (expression.subexpression[0],
                         str(expression.subexpression[1])))
 
@@ -117,9 +114,10 @@ class Expression:
             else:
                 break
 
-        # create code
-        code = ''
-        for expression in reversed(expressions):
+        # create a compound statement containing all subexpressions
+        code = '({\n'
+        for expression in reversed(subexpressions):
             code += '{} {} = {};\n'.format(dtype, expression[0], expression[1])
+        code += '{};\n}})'.format(self)
 
         return code
